632946a1223b109b0df8079c557c3c5dbe4fd57c
galyna-k <hkzlvs@gmail.com>
2025-02-03 23:55:02 +0000

    Create and add the Error boundery

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import PrivateRoute from "./routes/PrivateRoute";
 import { Dashboard, Home } from "./pages";
-import { ContainerLayout, Footer, Header } from "./components";
+import { ContainerLayout, ErrorBoundary, Footer, Header } from "./components";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +17,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary>
           <ContainerLayout>
           <Header searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
           <Routes>
@@ -28,7 +29,8 @@ export default function App() {
               } />
             </Routes>
             </ContainerLayout>
-          <Footer />
+            <Footer />
+          </ErrorBoundary>
         </Suspense>
       </Router>
     </QueryClientProvider>
