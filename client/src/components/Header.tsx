24168a5360e0a4c2ac49d302836eaf27ba889bce
galyna-k <hkzlvs@gmail.com>
2025-02-03 16:25:22 +0000

    Create the Header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import Auth from "./Auth";
+
+
+const Header: React.FC = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <header className="py-4 border-b-1 border-b-gray-300">
+      <div className="max-w-4xl mx-auto flex justify-between items-center px-4">
+          <Link to="/" className="text-xl font-bold">TODO</Link>
+        {isAuthenticated &&
+          <nav className="flex items-center gap-3">
+            <Link to="/dashboard">Board</Link>
+          </nav>
+        }
+
+        <Auth />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
