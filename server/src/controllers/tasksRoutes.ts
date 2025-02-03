2d41991416a7af11ed4be9383152bc23f39ece71
galyna-k <hkzlvs@gmail.com>
2025-02-03 15:44:30 +0000

    Add the request to search specific task

--- server/src/controllers/tasksRoutes.ts
+++ server/src/controllers/tasksRoutes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createTask, getTasks } from './tasks';
+import { createTask, getTasks, searchTasks } from './tasks';
 
 const router = express.Router();
 
 router.post('/', createTask);
 router.get('/', getTasks);
-
+router.get('/search', searchTasks);
 
 export default router;
