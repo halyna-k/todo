d1ad2fb260b4eef727fd8705792b90cd4a522f79
galyna-k <hkzlvs@gmail.com>
2025-02-03 16:01:29 +0000

    Update App.tsx
    add QueryClientProvider;
    add Router;

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-function App() {
+const queryClient = new QueryClient();
+
+export default function App() {
+
   return (
-    <div>
-
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={"Home"} />
+            <Route path="/dashboard" element={"Dashboard"} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </QueryClientProvider>
+  );
 }
-
-export default App
