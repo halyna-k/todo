78cb1dd735b4cb6b7207ad9bdb1d0401b97c966d
galyna-k <hkzlvs@gmail.com>
2025-02-03 21:36:58 +0000

    Create the Task management

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -9,3 +9,4 @@ export { default as SearchBar } from "./SearchBar";
 export { default as TaskCard } from "./TaskCard";
 export { default as TaskForm } from "./TaskForm";
 export { default as TaskList } from "./TaskList";
+export { default as TaskManagement } from "./TaskManagement";
