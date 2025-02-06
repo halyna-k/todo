cfb529dd88b5c82ff0ace5dac3820c6bffbe8bea
galyna-k <hkzlvs@gmail.com>
2025-02-06 11:52:53 +0000

    Correct using the check token and authenticate user

--- server/src/controllers/tasksRoutes.ts
+++ server/src/controllers/tasksRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createTask, getTasks, searchTasks } from './tasks';
+import { authenticateUser } from '../middleware/auth';
 
 const router = express.Router();
 
-router.post('/', createTask);
+router.post('/', authenticateUser, createTask);
 router.get('/', getTasks);
-router.get('/search', searchTasks);
+router.get('/search', authenticateUser, searchTasks);
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,20 +3,20 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import tasksRoutes from "./controllers/tasksRoutes";
-import { authenticateUser } from './middleware/auth';
+import { checkJwt, errorHandler } from './middleware/auth';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
-app.use('/tasks', authenticateUser, tasksRoutes);
+app.use('/tasks', checkJwt, tasksRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
