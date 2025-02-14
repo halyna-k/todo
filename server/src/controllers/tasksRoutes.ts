60430665a3b857c61e96f2afe7276f990832879b
galyna-k <hkzlvs@gmail.com>
2025-02-14 13:33:05 +0000

    Add a path to edit the task in the router

--- server/src/controllers/tasksRoutes.ts
+++ server/src/controllers/tasksRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, deleteTask, getTasks, searchTasks, updateTaskStatus } from './tasks';
+import { createTask, deleteTask, getTasks, searchTasks, updateTask, updateTaskStatus } from './tasks';
 import { authenticateUser } from '../middleware/auth';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/', authenticateUser, createTask);
 router.get('/', getTasks);
 router.get('/search', authenticateUser, searchTasks);
 router.patch('/:id/status', authenticateUser, updateTaskStatus);
+router.patch("/:id", authenticateUser, updateTask);
 router.delete('/:id', authenticateUser, deleteTask);
 
 export default router;
